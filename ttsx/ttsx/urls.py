c605824a36b8ddbc437df22c3c23627bae0f7f57
junjun <2538775661@qq.com>
2017-09-11 17:22:33 +0800

    改变

--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -16,10 +16,6 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('tt_goods.urls')),
-    url(r'^tinymce/', include('tinymce.urls')),
-    url(r'^user/',include('tt_user.urls')),
 ]
